22d790f231f8ac91f150ef5b2f2ba40c517316d3
Jonathan Irwin <jonoirwinrsa@gmail.com>
2025-05-13 17:30:23 -0400

    other: trigger build

--- cmd/zb/helper.go
+++ cmd/zb/helper.go
@@ -1028,25 +1028,6 @@ func getImageConfig() ([]byte, godigest.Digest) {
 	return configBlobContent, configBlobDigestRaw
 }
 
-//
-//func getLocation(baseURL string, resp *resty.Response) string {
-//	// For some API responses, the Location header is set and is supposed to
-//	// indicate an opaque value. However, it is not clear if this value is an
-//	// absolute URL (https://server:port/v2/...) or just a path (/v2/...)
-//	// zot implements the latter as per the spec, but some registries appear to
-//	// return the former - this needs to be clarified
-//	loc := resp.Header().Get("Location")
-//
-//	uloc, err := url.Parse(loc)
-//	if err != nil {
-//		return ""
-//	}
-//
-//	path := uloc.Path
-//
-//	return baseURL + path
-//}
-
 func getLocation(baseURL string, resp *resty.Response) string {
 	loc := resp.Header().Get("Location")
 	if strings.HasPrefix(loc, "http") {
